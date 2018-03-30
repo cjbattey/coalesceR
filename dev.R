ce1c4eaa3cccf6d3242526ae17ea497c63d25672
cjbattey <cjbattey@gmail.com>
2018-03-30 12:41:37 -0700

    i forget

--- dev.R
+++ dev.R
@@ -2,9 +2,9 @@ source("functions.R")
 
 sim <- coalesce(nsamples = 10,nsites=16000,sims=10000,cores=8,mu=2e-8)
 
-pdf(width=3,height=3,"sfs_ridge_plot_color.pdf")
+pdf(width=6,height=3,"sfs_ridge_plot_color_wide.pdf")
 #plot coalescent trees with edges colored by the number of descendent lineages
-plot_coal_tree(sim,n_col=3,n=9,edge_colors = T,mar=1,cex=1,bw=F,brewerpal="YlOrRd")
+plot_coal_tree(sim,n_col=6,n=18,edge_colors = T,mar=1,cex=1,pal="viridis",edge_width = 2.25)
 
 #get mutation generations
 muts <- do.call(rbind.data.frame,sim$mutations) 
@@ -13,18 +13,20 @@ med_mut_times <- ddply(muts,.(ndesc),summarize,med_gen=median(gen))
 
 #ridge plot for time distributions of mutations in each SFS class
 ridges <- ggplot(data=muts,aes(x=gen,y=factor(ndesc),fill=factor(ndesc)))+
-  #scale_fill_manual(values=grey.colors(max(muts$ndesc),start=0.1),guide=F)+
-  scale_fill_manual(values=rev(brewer.pal(max(muts$ndesc),"YlOrRd")),guide=F)+
+  #scale_fill_manual(values=grey.colors(max(muts$ndesc),start=0),guide=F)+
+  #scale_fill_manual(values=rev(brewer.pal(max(muts$ndesc),"BuGn")),guide=F)+
+  scale_fill_manual(values=viridis(max(muts$ndesc)),guide=F)+
   ylab("Derived Allele Count")+xlab("Mutation Time\n (generations before present)")+
   theme_bw()+theme(panel.grid=element_blank(),
                    axis.text = element_text(size=8),
                    axis.title = element_text(size=8))+
-  scale_x_continuous(breaks=c(0,6000,12000),limits=c(0,12000))+
+  scale_x_continuous(breaks=c(0,2500,5000,7500),limits=c(0,7500))+
   geom_density_ridges(lwd=0.25)+
   geom_segment(data=med_mut_times,aes(x=med_gen,y=ndesc,xend=med_gen,yend=ndesc-0.25),col="black",lwd=0.65)
 
-vp.right <- grid::viewport(just="right",width=0.65,x = 0.65)
+vp.right <- grid::viewport(just="right",width=0.5,x = 0.5)
 print(ridges,vp=vp.right)
+
 dev.off()
 
 
